86772b0d6be280815e0ce51cb3b56d43c19aa453
fieldr <fieldr92@gmail.com>
2019-01-03 14:37:45 +0000

    ✨ Add new top tracks feature - Component addition with API call within - CSS module added

---
+++ client/src/components/TopTracks/TopTracks.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import './TopTracks.css';
+
+class TopTracks extends Component {
+  state = {
+    tracks: null
+  }
+
+  componentDidMount() {
+    const url = 'https://api.spotify.com'
+    const accessToken = this.props.accessToken;
+
+    if (accessToken) {
+      fetch(`${url}/v1/me/top/tracks?limit=20`, {
+        headers: { 'Authorization': 'Bearer ' + accessToken }
+      }).then(res => res.json())
+      .then(data => {
+        this.setState({
+          tracks: data.items.map(item => ({
+            artists: item.artists.map(artist => (artist.name)),
+            name: item.name,
+            album: item.album.name,
+            albumArt: item.album.images[2].url
+          })
+        )});
+        console.log(this.state.tracks);
+      });
+    }
+  }
+
+  mapTracks = () => {
+    const { tracks } = this.state;
+
+    if (tracks) {
+      return tracks.map((track, i) => (
+          <li className='track-list' key={`track${i}`}>{track.name} - {track.artists.length > 1 ? track.artists.join(' / ') : track.artists}</li>
+        )
+      );
+    }
+  }
+
+  render() {
+    return (
+      <div className='track-container'>
+        <h3 className="playlists-title" style={{ textAlign: 'center' }}>Your top tracks</h3>
+        <ol>
+          {this.mapTracks()}
+        </ol>
+      </div>
+    )
+  }
+}
+
+export default TopTracks;

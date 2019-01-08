afdf5f2c3f4e6de24f00fd15249994ada8e9f791
fieldr <fieldr92@gmail.com>
2019-01-08 17:26:57 +0000

    🎨 Update title bar styling

--- client/src/components/TitleBar/TitleBar.js
+++ client/src/components/TitleBar/TitleBar.js
@@ -4,12 +4,12 @@ import './TitleBar.css';
 const TitleBar = ({ userData, clearUserData }) => {
   return (
     <div className='title-bar'>
-      <div className='title'>
-        {/* <img className='title-logo' src='https://developer.spotify.com/assets/branding-guidelines/icon1@2x.png' alt='Logo' /> */}
-        <h1 className='title'>
-          Ralph's Spotify App
-        </h1>
-      </div>
+      {/* <div className='title'>
+      </div> */}
+      <img className='title-logo' src='./assets/Spotify_Icon.png' alt='Logo' />
+      <h1 className='title'>
+        Ralph's Spotify App
+      </h1>
       <div style={{ textAlign: 'center' }}>
         <button onClick={() => {
           window.location = 'http://localhost:3000';
